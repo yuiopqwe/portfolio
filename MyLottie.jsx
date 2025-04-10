e5dadd922eb0298aa288be4fcf0b95c77646d4c1
yuiopqwe <yuiope90@naver.com>
2025-04-10 09:57:20 +0900

    s

--- MyLottie.jsx
+++ MyLottie.jsx
@@ -1,5 +1,5 @@
 import Lottie from "lottie-react";
-import animationData from "./your-animation.json";
+import animationData from "../json/1.json";
 
 function MyLottie() {
   return <Lottie animationData={animationData} loop={true} />;
